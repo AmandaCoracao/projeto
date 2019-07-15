d22c5d6a7ac8569a0ba5ded3d3f99ce1f2b5705e
AmandaCoracao <amandac.t@hotmail.com>
2019-07-15 16:18:51 -0300

    msg

---
+++ R/Introdução - Mexer no R.R
@@ -0,0 +1,18 @@
+# Para listar os objetos no workspace
+ls()
+
+#Para remover
+rm(list = ls())
+
+#Qual é a pasta de trabalho?
+getwd()
+
+# no terminal do RSTUDIO (onde vc pode fazer qualquer coisa e n está ligado ao R)
+#colocar pwd na aba terminal q significa print working directory
+# slide de configurar seu nome e e-mail para identificar q é seu
+# depois inicia uma "conexão" com o git hub colocando git init
+# criamos um repositório no github para o projeto e linkamos isso com o Rstudio pela criação de uma senha aqui SSH
+# pelo site, há um comando para enviar o que está salvo aqui para o diretório da nuvem do git hub
+# git remote -v para aprecer comandos, git add [arquivo] adcionar TAB para colocar a frma do arquivo (txt). 
+
+a <- c(5,4,3,2,1)
