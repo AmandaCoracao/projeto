5ddb10519c38435aeb29c745464d9e79ba41d9af
AmandaCoracao <amandac.t@hotmail.com>
2019-07-21 00:04:47 -0300

    exercicio1_organizardados

---
+++ R/exercicio1.R
@@ -0,0 +1,7 @@
+#exercicio01
+
+#temos que identificar o que há de errado na planilha de excel (formatação) e colocar tudo junto em apenas uma tabela e uma aba de excel.
+
+planilhamodificada <- read.csv("./data/exercicio_01_editado.csv")
+planilhamodificada
+
